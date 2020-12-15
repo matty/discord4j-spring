247e244d5ef9c44c4607d6d3a44a273450cde9ef
Matty Southall <matty@users.noreply.github.com>
2020-12-15 18:58:19 +0000

    Add first revision of DiscordChat annotation

---
+++ src/main/java/com/github/matty/discord4j/spring/annotations/DiscordChat.java
@@ -0,0 +1,19 @@
+package com.github.matty.discord4j.spring.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for incoming chat message.
+ *
+ * @author Matty Southall
+ * @since 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DiscordChat {
+
+    String filter();
+}
---
+++ src/main/java/com/github/matty/discord4j/spring/beans/DiscordChatBeanProcessor.java
@@ -0,0 +1,69 @@
+package com.github.matty.discord4j.spring.beans;
+
+import com.github.matty.discord4j.spring.annotations.DiscordChat;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.EventDispatcher;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+import reactor.core.publisher.Mono;
+
+import java.lang.reflect.Method;
+
+@Component("discordChatBeanProcessor")
+@ConditionalOnBean(GatewayDiscordClient.class)
+public class DiscordChatBeanProcessor implements BeanPostProcessor {
+
+    EventDispatcher eventDispatcher;
+
+    @Autowired
+    public DiscordChatBeanProcessor( EventDispatcher eventDispatcher) {
+        this.eventDispatcher = eventDispatcher;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        ReflectionUtils.doWithMethods(bean.getClass(), method -> {
+            if (method.isAnnotationPresent(DiscordChat.class)) {
+                doDiscordChat(method, bean);
+            }
+        });
+
+        return bean;
+    }
+
+    /**
+     * Methods annotated with {@link DiscordChat}.
+     */
+    @SuppressWarnings("unchecked")
+    private void doDiscordChat(Method method, Object bean) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        if (parameterTypes.length > 0) {
+            if (MessageCreateEvent.class.isAssignableFrom(parameterTypes[0])) {
+                doChatEvent(method, bean, (Class<MessageCreateEvent>) parameterTypes[0]);
+            }
+        }
+    }
+
+    private void doChatEvent(Method method, Object bean, Class<MessageCreateEvent> parameterClazz) {
+        DiscordChat discordChat = method.getAnnotation(DiscordChat.class);
+        eventDispatcher.on(parameterClazz)
+                .filter(m -> m.getMessage().getContent().equals(discordChat.filter()))
+                .flatMap(e -> {
+                    Object r = ReflectionUtils.invokeMethod(method, bean, e);
+                    if (r != null) {
+                        if (r instanceof Publisher) {
+                            return (Publisher<?>) r;
+                        }
+                    }
+
+                    return Mono.empty();
+                })
+                .subscribe();
+    }
+}
