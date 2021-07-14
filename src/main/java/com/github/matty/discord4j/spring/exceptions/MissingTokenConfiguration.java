b7b74b07dce8d6de790eb1927f9435ea7d8484fd
Matty Southall <matty@users.noreply.github.com>
2021-07-14 17:47:04 +0100

    Added MissingTokenConfiguration

---
+++ src/main/java/com/github/matty/discord4j/spring/exceptions/MissingTokenConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.matty.discord4j.spring.exceptions;
+
+/**
+ * Own exception for missing token configuration.
+ *
+ * @author Matty Southall
+ * @since 1.0
+ */
+public class MissingTokenConfiguration extends RuntimeException {
+}
